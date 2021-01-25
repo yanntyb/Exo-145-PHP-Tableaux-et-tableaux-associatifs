b597805697eb759bda886ad25afbc703342aecd6
yann tyb <yanntyb.lbc@gmail.com>
2021-01-25 14:50:59 +0100

    a

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
-
+$fruits = array("Apple", "Banana","Orange");
+echo count($fruits) . "<br>";
+echo $fruits[1];
 //Deuxieme ligne
 
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
+$age = array("Peter" =>"35", "Ben" =>"37", "Joe" =>"43");
 
-echo "<br><br>Ben is " .  . " years old.<br><br>";
+echo "<br><br>Ben is " . $age["Ben"] . " years old.<br><br>";
 
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x . ", Value=" . $y;
 }
 
 
@@ -21,13 +21,13 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+arsort($colors);
 
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+asort($age);
 
 
 
